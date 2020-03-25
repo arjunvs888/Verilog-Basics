02f7e0cfcb05c617d2f18fa3c3e33b750fcfd23b
deadsec <arjunvs888@gmail.com>
2020-03-25 20:09:22 +0530

    Full Sinewave from LUT
    Full Cycle of  Sinewave from LUT is generated with 1000 samples and  a Offset of 255

--- sine_generator/sine_generator.srcs/sources_1/new/sine_generator.v
+++ sine_generator/sine_generator.srcs/sources_1/new/sine_generator.v
@@ -1,18 +1,28 @@
-//Quarter Sine wave Generator
 `timescale 10ns / 1ps
-module sine_generator( input clk, output reg [8:0] sine_out);
-integer i = 0;
-reg[0:2] quad = 2'b00;
-parameter size = 1000;
+module sine_generator( input clk,output reg signed [8:0] sine_out=0);
+integer i=0;
+localparam size = 1000;
 //parameter clk_f = 50;
-reg [8:0] RAM [size-1:0];
+reg [0:1] x;
+reg signed [8:0] RAM [size-1:0];
 initial begin
     $readmemh("sin_lut.mem",RAM);
+    x=2'b00;
 end
 always@(posedge clk)begin
-        sine_out <= RAM[i];
-        $display ("Value1 %d:-->%h",i,sine_out);
-        i=i+1;
-        if(i==size-1)i<=0;
-        end
+        if(x==2'b00)begin
+            //i=0;
+            sine_out <= RAM[i]-255;
+            $display ("QUAD1 %d:-->%h",i,sine_out);
+            i=i+1;
+           end
+          if(i==size-1)i<=0;
+//       if(x==2'b01)begin
+//            //i=size-1;
+//            sine_out <= RAM[i];
+//            $display ("QUAD2 %d:-->%h",i,sine_out);
+//            i=i-1;
+//           end
+end   
+
 endmodule
